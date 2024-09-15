92eeb7bf0bff5db63393727b2715b04d3597ec83
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-09-15 21:07:35 +0530

    rendering content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/header/Header.jsx';
-import {CORE_CONCEPTS} from './data.js'
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-
+import Header from "./components/header/Header.jsx";
+import { CORE_CONCEPTS,EXAMPLES } from "./data.js";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("pleace click a button");
+  const [selectedTopic, setSelectedTopic] = useState();
 
-  function handleSelect(selectedButton){
-
+  function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     //selectedButton => 'components', 'jsx', 'props', 'state'
     //console.log(selectedButton)
-    
   }
 
-  console.log('APP COMPONENT EXECUTING')
+  console.log("APP COMPONENT EXECUTING");
+
+  let tabContent = <p>Pleace select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
@@ -31,20 +42,22 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <section id='examples'>
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')} >Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')} >JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')} >Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')} >State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {tabContent}
         </section>
       </main>
     </div>
