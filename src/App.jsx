2f46e3fec6219a0324dd85ddde7346ac51607302
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-09-15 19:05:48 +0530

    passing function as values

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+  function handleSelect(){
+    console.log('Hello World - selected!')
+  }
+
+
+
   return (
     <div>
       <Header/>
@@ -25,10 +31,10 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect} >Components</TabButton>
+            <TabButton onSelect={handleSelect} >JSX</TabButton>
+            <TabButton onSelect={handleSelect} >Props</TabButton>
+            <TabButton onSelect={handleSelect} >State</TabButton>
           </menu>
         </section>
       </main>
