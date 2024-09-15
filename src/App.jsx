e2263d12cc3fc0368751e42ddbc2199d8492c121
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-09-15 18:03:54 +0530

    adding header css in separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Header.jsx';
+import Header from './components/header/Header.jsx';
 import {CORE_CONCEPTS} from './data.js'
 import CoreConcept from './components/CoreConcept.jsx';
 
