10f193298db072243c0d47d316d8b17ae71917df
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-09-15 17:19:25 +0530

    creact the outputting dynamic-walues

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max){
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
 
 
-function Header(){
   return (
     <header>
       <img src="src\assets\react-core-concepts.png" alt="core congcept" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts will need for almost any app you are going to
+        {description} React concepts will need for almost any app you are going to
         build!
       </p>
     </header>
   );
 }
 
-
-
 function App() {
-
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <h2>Time to get started</h2>
       </main>
@@ -27,4 +31,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
